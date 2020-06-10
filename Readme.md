50520ce29a198a97b3b09228c47ad0dd0653ac99
ChrisChoke <secret@secret.de>
2020-06-10 23:28:35 +0200

    adding new device.json for getting friendlyName in mqtt topics, publish state to broker, handle some possible exceptions

--- Readme.md
+++ Readme.md
@@ -18,16 +18,42 @@ The `config.json` includes your configuration in json-format. The config in the 
 
 possible configs:
 
-```json
+```
 {
-"ssid" : "YOUR_SSID",
-"password" : "YOUR_PASSWORD",
-"machinePin" : 32,
-"mqttServer" : "IP_ADDRESS",
-"mqttPort" : 1111,              // default 1883
-"mqttUser" : "USERNAME",        // default None
-"mqttPasswd" : "PASSWORD",      // default None
-"ntp": "IP_ADDRESS or DNS",     // default None
-"topicPub" : "esp32/"           // default "esp32"
+    "ssid" : "YOUR_SSID",
+    "password" : "YOUR_PASSWORD",
+    "machinePin" : 32,
+    "mqttServer" : "IP_ADDRESS",
+    "mqttPort" : 1111,              // default 1883
+    "mqttUser" : "USERNAME",        // default None
+    "mqttPasswd" : "PASSWORD",      // default None
+    "ntp": "IP_ADDRESS or DNS",     // default None
+    "topicPub" : "esp32/"           // default "esp32/"
 }
 ```
+### devices.json
+
+The devices.json includes all connected ds18x20 sensors. The sensors will automaticliy append to this file on startup and you get information with mqtt message at esp32/system if new sensors are found.
+You can download devices.json from the board and put a friendlyName in the value section and put it back to the board, or creating a readable json file with the bytearray and a friendlyName and put it to the board. The bytearray you can copy from the mqtt message how explained above.
+Its recommend to do that, because the sensors will provide as bytearray. It looks a bit ugly for the mqtt topic and can generated another subtopic if there is a "/" in the bytearray.
+
+### MQTT
+
+you will get following mqtt topics:
+
+#### esp32/timestamp
+get a timestamp from last transmission to mqtt-broker formattet in:
+
+dd.mm.yyyy hh:mm:ss
+
+#### esp32/system/state
+get the connection state to mqtt-broker:</br>
+
+'Online' or </br>
+'Offline'
+
+#### esp32/system/linkquality
+get the wifi quality as rssi.
+
+#### esp32/friendlyName/temperature
+get the temperature of the ds18x20 sensors in °C
--- main.py
+++ main.py
@@ -1,42 +1,75 @@
+def connect_and_subscribe():
+  global clientId, mqttServer, topicSub
+  client = MQTTClient(clientId, mqttServer, port=mqttPort, user=mqttUser, password=mqttPasswd)
+  client.connect()
+  print('Connected to %s MQTT broker' % (mqttServer))
+  client.publish(topicPub+'system/state', 'Online')
+  return client
+
+def restart_and_reconnect():
+  try:
+    client.publish(topicPub+'system/state', 'Offline')
+  finally:
+    print('Rebooting. . .')
+    utime.sleep(10)
+    machine.reset()
+
+try:
+  client = connect_and_subscribe()
+except OSError as e:
+  print('An exception has occured: '+ str(e))
+  restart_and_reconnect()
+
 if ntp != None:
   ntptime.settime()
 dsPin = machine.Pin(machinePin)
 dsSensor = ds18x20.DS18X20(onewire.OneWire(dsPin))
 
 roms = dsSensor.scan()
+romsConvDict = { str(i) : str(i) for i in roms}
 print('Found DS devices: ', roms)
 
-def connect_and_subscribe():
-  global clientId, mqttServer, topicSub
-  client = MQTTClient(clientId, mqttServer, port=mqttPort, user=mqttUser, password=mqttPasswd)
-  client.connect()
-  print('Connected to %s MQTT broker' % (mqttServer))
-  return client
-
-def restart_and_reconnect():
-  print('Failed to connect to MQTT broker. Reconnecting...')
-  utime.sleep(10)
-  machine.reset()
-
 try:
-  client = connect_and_subscribe()
-except OSError as e:
-  restart_and_reconnect()
+    devicesRead = open('devices.json').read()
+except OSError:
+    createDevicesFile = open('devices.json', 'w')
+    createDevicesFile.write(ujson.dumps({}))
+    createDevicesFile.close()
+    devicesRead = open('devices.json').read()
+
+devicesJson = ujson.loads(devicesRead)
+print(devicesJson)
+for x in romsConvDict:
+  newDevicesPub = []
+  if x in devicesJson:
+    print(x + 'bereits in devices.json')
+  else:
+    devicesJson[x] = x
+    devicesAppend = open('devices.json', 'w')
+    devicesAppend.write(ujson.dumps(devicesJson))
+    devicesAppend.close()
+    newDevicesPub.append(x)
+    client.publish(topicPub+'system', 'New Sensors found and added to devices.json: '+ str(newDevicesPub))
 
 while True:
   try:
     dsSensor.convert_temp()
     utime.sleep_ms(750)
     for rom in roms:
-      serialnum = hex(int.from_bytes(rom, 'little'))
-      print(serialnum)
+      friendlyName = devicesJson[str(rom)] if str(rom) in devicesJson else str(rom)
+      print(friendlyName)
       print(dsSensor.read_temp(rom))
       if ntp != None:
         timestamp = utime.localtime()
         client.publish(topicPub+'timestamp', str('{:02}'.format(timestamp[2]))+'.'+str('{:02}'.format(timestamp[1]))+'.'+str(timestamp[0])+' '+str('{:02}'.format(timestamp[3]+2))+':'+str('{:02}'.format(timestamp[4]))+':'+str('{:02}'.format(timestamp[5])))
       else:
         client.publish(topicPub+'timestamp', 'ntp not defined')
-      client.publish(topicPub+serialnum+'/temperature', str(dsSensor.read_temp(rom)))
+      client.publish(topicPub+friendlyName+'/temperature', str(dsSensor.read_temp(rom)))
+      client.publish(topicPub+'system/linkquailty', str(station.status('rssi')))
     utime.sleep(5)
   except OSError as e:
+    print('An exception has occured: '+ str(e))
+    restart_and_reconnect()
+  except onewire.OneWireError as e:
+    print('An exception has occured: '+ repr(e))
     restart_and_reconnect()
